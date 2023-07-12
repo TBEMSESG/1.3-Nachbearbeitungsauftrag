4612b78077949c134a0f90dda76af210cafd619e
Thomas <tbe@pm.me>
2023-07-12 21:00:45 +0200

    done

--- aufgabe3.js
+++ aufgabe3.js
@@ -1,17 +1,24 @@
-function istPangramm(word) {
+let res3 = document.getElementById("result3");
+
+function istPangramm() {
+  let word3 = document.getElementById("c").value;
+
   let alphabet = "abcdefghijklmnopqrstuvwxyz ";
-  let wordsmall = word.toLowerCase();
+  let wordsmall = word3.toLowerCase();
 
   for (i = 0; i < alphabet.length; i++) {
     let char = true;
     char = wordsmall.includes(alphabet[i]);
-    if (char == false)
-      return console.log(
-        `the letter "${alphabet[i]}" is not in "${word}". Sorry, not a Pangramm`
+    if (char == false) {
+      res3.textContent = `the letter "${alphabet[i]}" is not in "${word3}". Sorry, not a Pangramm`;
+      console.log(
+        `the letter "${alphabet[i]}" is not in "${word3}". Sorry, not a Pangramm`
       );
+      return;
+    }
   }
-
-  console.log(`YESSSS!!!! The string "${word}" is a Pangramm !!`);
+  res3.textContent = `YESSSS!!!! The string "${word3}" is a Pangramm !!`;
+  console.log(`YESSSS!!!! The string "${word3}" is a Pangramm !!`);
 }
 
-istPangramm("The quick brown fox jumps over the lazy dog");
+// istPangramm("The quick brown fox jumps over the lazy dog");
